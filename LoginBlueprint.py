47d88f770c59051c00a07c1b0443e0bc7a741403
Angel Orsinado <orsinadoaaa033115@gmail.com>
2018-10-21 18:43:12 +0800

    Added login blueprint

---
+++ LoginBlueprint.py
@@ -0,0 +1,44 @@
+from flask import Blueprint,render_template,session,redirect,url_for,request
+from extensions import mysql
+
+
+login_blueprint = Blueprint('login_blueprint', __name__)
+
+
+# User login
+@login_blueprint.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Get Form Fields
+        email = request.form['email']
+        password_candidate = request.form['password']
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        # Get user by email
+        result = cur.execute("SELECT * FROM user_registration WHERE email = %s", [email])
+
+        if result > 0:
+            # Get stored hash
+            data = cur.fetchone()
+            password = data['password']
+
+            # Compare Passwords
+            if sha256_crypt.verify(password_candidate, password):
+                # Passed
+                session['logged_in'] = True
+                session['email'] = email
+
+                flash('You are now logged in', 'success')
+                return redirect(url_for('dashboard'))
+            else:
+                error = 'Invalid login'
+                return render_template('login.html', error=error)
+            # Close connection
+            cur.close()
+        else:
+            error = 'User not found'
+            return render_template('login.html', error=error)
+
+    return render_template('login.html')
